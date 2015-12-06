61ee20b6706d1f8074b6769249dbd1bbb27a29a5
VeskoMitev <vesko.mitev@gmail.com>
2015-12-06 15:09:22 +0200

    :blue_book: Homework

---
+++ Problem_02_BonusScore/BonusScore.cs
@@ -0,0 +1,41 @@
+﻿namespace ConditionalStatements
+{
+    using System;
+    /*
+     * Problem 2.	Bonus Score
+
+     * Write a program that applies bonus score to given score in the range [1…9] by the following rules:
+        •	If the score is between 1 and 3, the program multiplies it by 10.
+        •	If the score is between 4 and 6, the program multiplies it by 100.
+        •	If the score is between 7 and 9, the program multiplies it by 1000.
+        •	If the score is 0 or more than 9, the program prints “invalid score”.
+     */ 
+
+    class BonusScore
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter score:");
+            double score = double.Parse(Console.ReadLine());
+
+            if (score > 0 && score < 4)
+            {
+                score *= 10;
+            }
+            else if (score > 3 && score < 7)
+            {
+                score *= 100;
+            }
+            else if (score > 6 && score < 10)
+            {
+                score *= 1000;
+            }
+            else
+            {
+                Console.WriteLine("invalid score");
+                return;
+            }
+            Console.WriteLine(score);
+        }
+    }
+}
