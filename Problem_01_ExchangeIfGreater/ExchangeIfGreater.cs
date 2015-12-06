e82519e417ed6fd65a963deadd2da95bc9f81c3c
VeskoMitev <vesko.mitev@gmail.com>
2015-12-06 14:49:14 +0200

    Homework

--- Problem_01_ExchangeIfGreater/ExchangeIfGreater.cs
+++ Problem_01_ExchangeIfGreater/ExchangeIfGreater.cs
@@ -5,7 +5,18 @@
     {
         static void Main()
         {
-//test
+            Console.Write("Enter first number:");
+            double firstNumber = double.Parse(Console.ReadLine());
+            Console.Write("Enter second number:");
+            double secondNumber = double.Parse(Console.ReadLine());
+            if (firstNumber>secondNumber)
+            {
+                Console.WriteLine("{0} {1}",secondNumber,firstNumber);
+            }
+            else
+            {
+                Console.WriteLine("{0} {1}", firstNumber,secondNumber ); 
+            }
         }
     }
 }
