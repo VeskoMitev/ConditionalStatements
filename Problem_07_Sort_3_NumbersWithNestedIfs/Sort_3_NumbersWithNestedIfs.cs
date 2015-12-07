71d53bad77b579049b2cebdbe27bf38b309c8991
VeskoMitev <vesko.mitev@gmail.com>
2015-12-07 10:54:05 +0200

    :blue_book: Homework

---
+++ Problem_07_Sort_3_NumbersWithNestedIfs/Sort_3_NumbersWithNestedIfs.cs
@@ -0,0 +1,47 @@
+﻿namespace ConditionalStatements
+{
+    using System;
+    /*
+     * Problem 7.	Sort 3 Numbers with Nested Ifs
+
+     * Write a program that enters 3 real numbers and prints them
+     * sorted in descending order. Use nested if statements. 
+     * Don’t use arrays and the built-in sorting functionality. 
+     */ 
+
+    class Sort_3_NumbersWithNestedIfs
+    {
+        static void Main(string[] args)
+        {
+            decimal maxNumber = decimal.MinValue;
+            decimal minNumber = decimal.MaxValue;
+            decimal midNumber = decimal.MinValue;
+            decimal inputValue = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Enter number {0}:", i + 1);
+                inputValue = decimal.Parse(Console.ReadLine());
+                bool isMid = true;
+                if (inputValue > maxNumber)
+                {
+                   midNumber = maxNumber;
+                   maxNumber = inputValue;
+                }
+                if (inputValue < minNumber)
+                {
+                    midNumber = minNumber;
+                    minNumber = inputValue;
+                }
+                if ((inputValue > minNumber && inputValue < maxNumber) || maxNumber == minNumber)
+                {
+                    midNumber = inputValue;
+                }
+                
+            }
+            Console.WriteLine(maxNumber);
+            Console.WriteLine(midNumber);
+            Console.WriteLine(minNumber);
+
+        }
+    }
+}
---
+++ Problem_08_DigitAsWord/DigitAsWord.cs
@@ -0,0 +1,58 @@
+﻿namespace ConditionalStatements
+{
+    using System;
+    /*
+     * Problem 8.	Digit as Word
+
+     * Write a program that asks for a digit (0-9), and depending on the input, 
+     * shows the digit as a word (in English). 
+     * Print “not a digit” in case of invalid inut. Use a switch statement
+     */ 
+
+    class DigitAsWord
+    {
+        static void Main()
+        {
+           Console.Write("Enter integer number:");
+            int inputData = Int32.Parse(Console.ReadLine());
+            string result = "";
+            switch (inputData)
+            {
+                case 0:
+                    result = "zero";
+                    break;
+                case 1:
+                    result = "one";
+                    break;
+                case 2:
+                    result = "two";
+                    break;
+                case 3:
+                    result = "three";
+                    break;
+                case 4:
+                    result = "four";
+                    break;
+                case 5:
+                    result = "five";
+                    break;
+                case 6:
+                    result = "six";
+                    break;
+                case 7:
+                    result = "seven";
+                    break;
+                case 8:
+                    result = "eight";
+                    break;
+                case 9:
+                    result = "nine";
+                    break;
+                default :
+                    result = "not a digit";
+                    break;
+            }
+            Console.WriteLine(result );
+        }
+    }
+}
