fd8f257ef89777e3e8b319198cdafa1c3b4660c2
VeskoMitev <vesko.mitev@gmail.com>
2015-12-08 10:19:01 +0200

    Homework

---
+++ Problem_09_PlayВithIntDoubleАndString/PlayВithIntDoubleАndString.cs
@@ -0,0 +1,45 @@
+﻿namespace ConditionalStatements
+{
+
+    using System;
+        /*
+         * Problem 9.	Play with Int, Double and String
+
+         * Write a program that, depending on the user’s choice, inputs an int,
+         * double or string variable. If the variable is int or double,
+         * the program increases it by one. If the variable is a string,
+         * the program appends "*" at the end. Print the result at the console. 
+         * Use switch statement. 
+         */
+
+   class PlayВithIntDoubleАndString
+    {
+        static void Main()
+        {
+            Console.WriteLine("1 --> int");
+            Console.WriteLine("2 --> double");
+            Console.WriteLine("3 --> string");
+            Console.Write("Please choose a type:");
+            ConsoleKeyInfo  enterKey=Console.ReadKey();
+            Console.WriteLine();
+            switch (enterKey.KeyChar)
+            {
+                case '1': 
+                    Console.Write("Please enter a integer:");
+                    Console.WriteLine(Console.ReadLine());
+                    break;
+                case '2':
+                    Console.Write("Please enter a double:");
+                    Console.WriteLine(Console.ReadLine());
+                    break;
+                case '3':
+                    Console.Write("Please enter a string:");
+                    Console.WriteLine("{0}{1}",Console.ReadLine(),"*");
+                   break;
+                default:
+                    break;
+
+            }
+        }
+    }
+}
---
+++ Problem_10_BeerTime/BeerTime.cs
@@ -0,0 +1,47 @@
+﻿namespace Problem_10_BeerTime
+{
+    using System;
+
+    /*
+     * Problem 10.	* Beer Time
+
+     * A beer time is after 1:00 PM and before 3:00 AM. 
+     * Write a program that enters a time in format “hh:mm tt” 
+     * (an hour in range [01...12], a minute in range [00…59] and AM / PM designator)
+     * and prints “beer time” or “non-beer time” according to the definition above or 
+     * “invalid time” if the time cannot be parsed. 
+     * Note that you may need to learn how to parse dates and times
+     */ 
+
+    class BeerTime
+    {
+        static void Main()
+        {  
+            TimeSpan inputTime;
+            DateTime inputDataTime;
+            TimeSpan startBeerTime = DateTime.Parse("01:00 PM").TimeOfDay;
+            TimeSpan endBeerTime = DateTime.Parse("3:00 AM").TimeOfDay ;
+
+            Console.Write("Enters a time in format \"hh:mm tt\": ");
+
+            string inputTimeString = Console.ReadLine();
+
+            bool isValidTime = DateTime.TryParse(inputTimeString, out inputDataTime);
+            if (isValidTime == false)
+            {
+                Console.WriteLine("invalid time");
+                return;
+            }
+            inputTime = inputDataTime.TimeOfDay;
+           
+            if (TimeSpan.Compare(inputTime, startBeerTime) >= 0 && TimeSpan.Compare(inputTime, endBeerTime) > 0)
+            {
+                Console.WriteLine("beer time");
+            }
+            else
+            {
+               Console.WriteLine("non-beer time");
+            }
+        }
+    }
+}
